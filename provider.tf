b2eabed1d6229af37b98688a23634c7588d0b253
cpg4590 <chandrapal31@outlook.com>
2025-08-02 18:01:00 +0530

    done

--- provider.tf
+++ provider.tf
@@ -1,17 +1,25 @@
 terraform {
   required_providers {
     azurerm = {
-        source = "hashicorp/azurerm"
-        version = "3.36.0"
-        version = "4.38.1"
-        version = "4.37.0"
-       
+      source  = "hashicorp/azurerm"
+      version = "3.36.0"
+      version = "4.38.1"
+      version = "4.37.0"
+
     }
+  }
+  backend "azurerm" {
+    storage_account_name = "hcl9999999"
+    container_name       = "hclcontainer"
+    resource_group_name  = "hcl-rg"
+    key                  = "cp-tfstate"
+
   }
 }
 
+
 provider "azurerm" {
-    features {} 
-    subscription_id = "a31b354c-d961-43d8-b6c2-57a22c72a3db"
-  
+  features {}
+  subscription_id = "a31b354c-d961-43d8-b6c2-57a22c72a3db"
+
 }
--- rg.tf
+++ rg.tf
@@ -1,4 +1,4 @@
 resource "azurerm_resource_group" "myrg" {
-    name = "hcl-rg"
+  name     = "hcl-rg"
   location = "east us"
 }
